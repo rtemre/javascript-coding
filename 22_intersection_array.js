8544da70859a2b5ad994999ee2b806188e1d26ef
Raman Temre <ramantemre2110@gmail.com>
2025-09-24 13:17:23 +0530

    Intersection array using set

--- 22_intersection_array.js
+++ 22_intersection_array.js
@@ -17,3 +17,15 @@ function intersectionWithoutMethod(arr1, arr2) {
   return result;
 }
 console.log(intersectionWithoutMethod(arr1, arr2));
+
+var intersection = function (nums1, nums2) {
+  const set = new Set(nums1);
+  return nums2.filter((value) => set.delete(value));
+};
+
+const a1 = [1, 2, 2, 1],
+  a2 = [2, 2];
+console.log(intersection(a1, a2)); // [2]
+const a3 = [4, 9, 5],
+  a4 = [9, 4, 9, 8, 4];
+console.log(intersection(a3, a4)); // [9,4]
